72e2486ca04f58d926ee14ff60dcf5f897247135
Garrett Nay <garrettnay@ymail.com>
2014-06-25 17:51:25 +0000

    refactor: clean up stuff from Yeoman generator

--- README.md
+++ README.md
@@ -8,24 +8,3 @@ Download the [production version][min] or the [development version][max].
 
 [min]: https://raw.github.com/garrettn/jquery-takeout/master/dist/jquery.takeout.min.js
 [max]: https://raw.github.com/garrettn/jquery-takeout/master/dist/jquery.takeout.js
-
-In your web page:
-
-```html
-<script src="jquery.js"></script>
-<script src="dist/takeout.min.js"></script>
-<script>
-jQuery(function($) {
-  $.awesome(); // "awesome"
-});
-</script>
-```
-
-## Documentation
-_(Coming soon)_
-
-## Examples
-_(Coming soon)_
-
-## Release History
-_(Nothing yet)_
--- src/takeout.js
+++ src/takeout.js
@@ -8,31 +8,11 @@
 
 (function ($) {
 
-  // Collection method.
-  $.fn.awesome = function () {
+  $.fn.takeout = function () {
     return this.each(function (i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+      
+
     });
-  };
-
-  // Static method.
-  $.awesome = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
-
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
-
-  // Custom selector.
-  $.expr[':'].awesome = function (elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
   };
 
 }(jQuery));
